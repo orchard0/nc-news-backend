1eb6f2d379a806823e62572ceb9a43b482fdaf01
arl <arlatif@pm.me>
2023-11-23 17:15:24 +0000

    fix git

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -110,7 +110,6 @@ describe('GET /api/articles/', () => {
 						created_at: expect.any(String),
 						votes: expect.any(Number),
 						article_img_url: expect.any(String),
-						comment_count: expect.any(Number),
 					});
 				}
 				expect(articles).toBeSortedBy('created_at', {
