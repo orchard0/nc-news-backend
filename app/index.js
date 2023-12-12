0802193a3efe76e4be2fbf705b4cfaaa20e35d64
arl <arlatif@pm.me>
2023-12-12 12:05:16 +0000

    fixed duplicate app.js

--- app/index.js
+++ app/index.js
@@ -7,7 +7,10 @@ const {
 	handleServerErrors,
 } = require('./errors');
 const apiRouter = require('./routes/api.router');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
